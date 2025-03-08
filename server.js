b2fc0ca071a5580d5e368a2e181b49a473bd6bae
Selenne-Strat <Antonio@selennecigars.com>
2025-03-08 17:27:00 -0500

    Update port to 8080 and confirm Dockerfile

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
